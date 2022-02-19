b1bd4114983b73c6d79b687a6cb3f3adb9c629d8
Criepstar <criepstar@gmail.com>
2022-02-19 09:30:38 +0100

    loader

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,37 @@
+
+<script lang="ts" context="module">
+    import axios from "axios"
+import { each } from "svelte/internal";
+    export async function load(){
+        try{
+            const data = await axios.get("https://preview.pesmarica.at/data")
+            if(data){
+                return{
+                props:{
+                    data: data.data
+                }
+            } 
+            }else{
+                return{
+                    props:{
+                        error: true
+                    }
+                }
+            }
+        }catch(err){
+            return {
+                props:{
+                    error: true
+                }
+            }
+        }
+    }
+</script>
+
 <script lang="ts">
     import {t, locale, loadTranslations} from "../lib/translations"
+
+    export let data;
 
     async function onStart(){
         const defaultLocale = "sl";
@@ -10,7 +42,9 @@
     onStart()
 </script>
 
-
 <h1>Welcome to SvelteKit</h1>
 <h2>{$t('home.test')}</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#each data as document}
+    <p>{document.data.title}</p>
+{/each}
